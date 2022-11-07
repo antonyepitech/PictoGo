e500268d0227fbcd013f6e229ae866f980dab869
anthony <anthony.cordon@epitech.eu>
2022-11-07 14:36:57 +0100

    feat: init project add component(doesn't work)

--- Front/PictoGo/src/app/app-routing.module.ts
+++ Front/PictoGo/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '',
+  { path: 'home',
     loadChildren: () => import('./module-principal/module-principal.module').then(m => m.ModulePrincipalModule)
   },
 ];
