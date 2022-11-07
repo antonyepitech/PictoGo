3b368a51a74e3cc8b16e11d8387edd0c0331e075
anthony <anthony.cordon@epitech.eu>
2022-11-07 12:05:36 +0100

    feat: server stared

--- hello.go
+++ hello.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+type NewAggPage struct {
+	Title string
+	News  string
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewAggPage{Title: "AMAZING", News: "OK"}
+	t, _ := template.ParseFiles("hello.html")
+	t.Execute(w, p)
 	fmt.Println(w, "so great !")
 }
 func main() {
