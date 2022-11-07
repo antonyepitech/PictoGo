f9cf26ca40037aa0014316a563e31bf0ad6bf811
anthony <anthony.cordon@epitech.eu>
2022-11-07 14:42:15 +0100

    fix: bug view done

--- Front/PictoGo/src/app/module-principal/home/home.component.ts
+++ Front/PictoGo/src/app/module-principal/home/home.component.ts
@@ -7,35 +7,13 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  postEmail: any;
-  postName: any;
-  postSubject: any;
-  postMessage: any;
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {}
   // tslint:disable-next-line:typedef
-  clickbutton(e: Event){
-    const form: any = e.currentTarget;
-    const headers = {"Content-Type": "application/x-www-form-urlencoded"};
-    const body = {
-      email: form.elements.namedItem("email").value,
-      name: form.elements.namedItem("name").value,
-      subject: form.elements.namedItem("subject").value,
-      message: form.elements.namedItem("message").value,
-    };
-    console.log("toto", body);
-    this.http.post<any>('core/sendEmail.php', body, {headers})
-      .subscribe(data => {
-        console.log(data);
-        alert(data);
-      }, error => {
-        console.error(error);
-      });
-    e.preventDefault();
-  }
+
 }
 
